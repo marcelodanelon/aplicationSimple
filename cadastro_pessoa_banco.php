551352fff600ade19aa4d18d5f67a8ac14d1024f
marcelodanelon <marcelodanelon11@gmail.com>
2023-03-13 21:39:08 -0300

    opcao de upload foto

--- cadastro_pessoa_banco.php
+++ cadastro_pessoa_banco.php
@@ -12,8 +12,16 @@
     $telefone = $_POST['telefone'];
     $senha = $_POST['senha'];
     $senha = md5($senha);
+    $foto = $_FILES['foto']['name'];
 
-    $slq = "insert into clientes (cpf,nome,rua,numero,bairro,cidade,estado,email,telefone,senha) values ('$cpf', '$nome','$rua','$numero','$bairro','$cidade','$estado','$email','$telefone','$senha')";
+    $pasta = "img/";
+
+    $partes = explode(".",$foto); 
+    $nomeNovo =  round(microtime(true)) . ".". end($partes); 
+
+    move_uploaded_file($_FILES["foto"]["tmp_name"],$pasta . $nomeNovo);
+
+    $slq = "insert into clientes (cpf,nome,rua,numero,bairro,cidade,estado,email,telefone,senha,foto) values ('$cpf', '$nome','$rua','$numero','$bairro','$cidade','$estado','$email','$telefone','$senha','$nomeNovo')";
 
     mysqli_query($conexao,$slq);
 
